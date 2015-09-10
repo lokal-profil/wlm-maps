35757949df1be00054f46123b2e26bececbc5723
lokal-profil <lokal.profil@gmail.com>
2015-09-10 12:17:38 +0200

    whitespace trimming and splitting monument streams

--- ajaxmonuments.php
+++ ajaxmonuments.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 /* Original code: https://github.com/chillly/plaques/blob/master/ajax/ajxplaques.php
- * 
+ *
  * Created by Chris Hill <osm@raggedred.net> and contributors.
  * Adapted for Wiki Loves Monuments by Emijrp <emijrp@gmail.com>
- * 
+ *
  * This software and associated documentation files (the "Software") is
  * released under the CC0 Public Domain Dedication, version 1.0, as
  * published by Creative Commons. To the extent possible under law, the
  * author(s) have dedicated all copyright and related and neighboring
  * rights to the Software to the public domain worldwide. The Software is
  * distributed WITHOUT ANY WARRANTY.
- * 
+ *
  * If you did not receive a copy of the CC0 Public Domain Dedication
  * along with the Software, see
  * <http://creativecommons.org/publicdomain/zero/1.0/>
  */
- 
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 /*
  * ajaxmonuments.php
- * returns monuments points as geojson 
+ * returns monuments points as geojson
  */
 
 if (isset($_GET['bbox'])) {
@@ -39,6 +39,10 @@ list($left,$bottom,$right,$top)=explode(",",$bbox);
 $mobile='0';
 if (isset($_GET['mobile'])) {
     $mobile = $_GET['mobile'];
+}
+$withImages='0';
+if (isset($_GET['withImages'])) {
+    $withImages = $_GET['withImages'];
 }
 // open the database
 $dbmycnf = parse_ini_file("../replica.my.cnf");
@@ -64,10 +68,15 @@ try {
     if ($mobile == '1'){
         $limit = 50;
     }
+    $imageCondition = "image == ''";
+    if ($withImages == '1'){
+        $imageCondition = "image != ''";
+    }
     $sql = "SELECT country, lang, id, name, lat, lon, image, commonscat, " .
            "monument_article, monument_random FROM monuments_all " .
            "WHERE lon>=:left AND lon<=:right AND lat>=:bottom AND lat<=:top AND " .
-           "country like 'se-%' ORDER BY monument_random LIMIT " . $limit;
+           "country like 'se-%' AND " . $imageCondition . " " .
+           "ORDER BY monument_random LIMIT " . $limit;
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':left', $left, PDO::PARAM_STR);
     $stmt->bindParam(':right', $right, PDO::PARAM_STR);
@@ -86,35 +95,41 @@ try {
 $ajaxres=array(); // place to store the geojson result
 $features=array(); // array to build up the feature collection
 $ajaxres['type']='FeatureCollection';
+$ajaxres['withImage']=$withImages;
 
-// go through the list adding each one to the array to be returned    
+// go through the list adding each one to the array to be returned
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $lat=$row['lat'];
     $lon=$row['lon'];
-    
+
     $prop=array();
     $prop['country']=$row['country'];
+    $prop['municipality']=$row['municipality'];
+    $prop['address']=$row['address'];
     $prop['lang']=$row['lang'];
     $prop['id']=$row['id'];
     $prop['name']=explode('|', $row['name']);
     $prop['name']=str_replace(array('[', ']'), '', $prop['name'][0]);
     $prop['monument_article']=str_replace(' ' , '_', $row['monument_article']);
     $prop['image']=str_replace(' ', '_', $row['image']);
+    $prop['image']=preg_replace('/_+/', '_', $prop['image']); //remove duplicated spaces
+    $prop['image']=preg_replace('/([Ff]ile|[Ii]mage):/', '', $prop['image']);
     if (empty($prop['image'])) {
         $prop['image'] = 'Monument_unknown.png';
     }
     $prop['md5']=substr(md5($prop['image']),0,2);
     $prop['commonscat']=str_replace(' ', '_', $row['commonscat']);
+    $prop['source']=str_replace(' ', '_', preg_split('/&[a-z]+=/', $row['source'])[0]);
 
     $f=array();
     $geom=array();
     $coords=array();
-    
+
     $geom['type']='Point';
     $coords[0]=floatval($lon);
     $coords[1]=floatval($lat);
     $geom['coordinates']=$coords;
-    
+
     $f['type']='Feature';
     $f['geometry']=$geom;
     $f['properties']=$prop;
